d03bbc9f7d036185666392c89255a0b436d2d641
tomha <tomhaug75@gmail.com>
2021-12-04 08:36:23 -0500

    refactor day03 part02

--- day03/day03_part02.py
+++ day03/day03_part02.py
@@ -1,59 +1,46 @@
 import pandas as pd
+import operator
+from typing import Callable
 import sys
 sys.path.append("..")
-from shared.utils import load_text_file
+from shared.utils import load_int_data_frame_from_file, bit_list_to_number
+
+
+ComparerFunc = Callable[[int, int], bool]
+
+
+class RatingCalculator:
+    def __init__(self, comparer_func: ComparerFunc):
+        self.comparer_func = comparer_func
+
+    def calculate_rating(self, df_all_values: pd.DataFrame,) -> int:
+        df_filtered = self._filter_data_frame(df_all_values, 0)
+        bit_list = df_filtered.values.tolist()[0]
+        return bit_list_to_number(bit_list)
+
+    def _filter_data_frame(self, df: pd.DataFrame, position: int) -> pd.DataFrame:
+        filter_value = self._calculate_value_at_position(df, position)
+        df_filtered = df[df[position] == filter_value]
+        if len(df_filtered) == 1:
+            return df_filtered
+        else:
+            return self._filter_data_frame(df_filtered, position + 1)
+
+    def _calculate_value_at_position(self, df: pd.DataFrame, position: int) -> int:
+        groups = df.groupby(position).groups
+        return 1 if self.comparer_func(groups[1].size, groups[0].size) else 0
+
+
+oxygen_rating_calculator = RatingCalculator(operator.ge)
+co2_scrubber_calculator = RatingCalculator(operator.lt)
 
 
 def get_part_two_result(file_name: str) -> int:
-    lines = load_text_file(file_name)
-    df_all_values = pd.DataFrame([[int(char) for char in line] for line in lines])
-
-    oxygen_generator_rating = calculate_rating(df_all_values, oxygen_generator)
-    co2_scrubber_rating = calculate_rating(df_all_values, co2_scrubber)
+    df_all_values = load_int_data_frame_from_file(file_name)
+    oxygen_generator_rating = oxygen_rating_calculator.calculate_rating(df_all_values)
+    co2_scrubber_rating = co2_scrubber_calculator.calculate_rating(df_all_values)
     life_support_rating = oxygen_generator_rating * co2_scrubber_rating
-
     return life_support_rating
-
-
-def calculate_rating(df_all_values: pd.DataFrame, comparer):
-    df_filtered = filter_data_frame(df_all_values, 0, comparer)
-    bit_list = df_filtered.values.tolist()[0]
-    return bit_list_to_number(bit_list)
-
-
-def filter_data_frame(df: pd.DataFrame, position: int, comparer):
-    filter_value = most_common_value_at_position(df, position, comparer)
-    df_filtered = df[df[position] == filter_value]
-    if len(df_filtered) == 1:
-        return df_filtered
-    else:
-        return filter_data_frame(df_filtered, position + 1, comparer)
-
-
-def most_common_value_at_position(df: pd.DataFrame, position: int, comparer) -> int:
-    groups = df.groupby(position).groups
-    return comparer(groups[0].size, groups[1].size)
-
-
-def oxygen_generator(num_zeros: int, num_ones: int):
-    if num_ones >= num_zeros:
-        return 1
-    else:
-        return 0
-
-
-def co2_scrubber(num_zeros: int, num_ones: int):
-    if num_ones < num_zeros:
-        return 1
-    else:
-        return 0
-
-
-def bit_list_to_number(bitlist: list[int]):
-    out = 0
-    for bit in bitlist:
-        out = (out << 1) | bit
-    return out
 
 
 if __name__ == "__main__":
--- shared/utils.py
+++ shared/utils.py
@@ -1,10 +1,16 @@
 import os
 import sys
+import pandas as pd
 
 
 def load_int_list_from_file(file_name: str) -> list[int]:
     file_contents = load_text_file(file_name)
     return [int(line) for line in file_contents]
+
+
+def load_int_data_frame_from_file(file_name: str) -> pd.DataFrame:
+    file_contents = load_text_file(file_name)
+    return pd.DataFrame([[int(char) for char in line] for line in file_contents])
 
 
 def load_text_file(file_name: str) -> list[str]:
@@ -13,3 +19,11 @@ def load_text_file(file_name: str) -> list[str]:
     file_contents = f.read()
     f.close()
     return file_contents.splitlines()
+
+
+def bit_list_to_number(bit_list: list[int]):
+    result = 0
+    for bit in bit_list:
+        result = (result << 1) | bit
+    return result
+
