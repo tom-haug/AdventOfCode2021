aad8725663cd4f22472a7f6c5a38ab8d0e06b936
tomha <tomhaug75@gmail.com>
2021-12-15 21:11:10 -0500

    day 15 refactor

--- src/day15/part01.py
+++ src/day15/part01.py
@@ -1,43 +1,6 @@
-import numpy as np
-from src.pyastar.interface import astar_graph
-from src.pyastar.util.graph import WeightedGraph
-
-from src.shared.utils import load_int_nparray_from_file
-
-
-def to_graph(matrix: np.ndarray):
-    G = WeightedGraph(undirected=True)
-    height, width = matrix.shape
-
-    for x in range(width):
-        for y in range(height):
-            G.add_node((x, y))
-            cost = matrix[y, x]
-            if x - 1 >= 0:
-                G.add_edge((x - 1, y), (x, y), cost)
-            if y + 1 < height:
-                G.add_edge((x, y + 1), (x, y), cost)
-            if x + 1 < width:
-                G.add_edge((x + 1, y), (x, y), cost)
-            if y - 1 >= 0:
-                G.add_edge((x, y - 1), (x, y), cost)
-
-    G.preprocess()
-    return G
-
-
-def get_part_one_result(file_name: str):
-    matrix = load_int_nparray_from_file(file_name)
-    start = (0, 0)
-    end = tuple(dimension - 1 for dimension in matrix.shape)
-    graph = to_graph(matrix)
-
-    ret = astar_graph(graph, start, end)
-    cost = ret[1]
-    print("Shortest Path:", ret[0])
-    return cost
+from src.day15.common import process
 
 
 if __name__ == "__main__":
-    result = get_part_one_result("src/day15/input.txt")
+    result = process("src/day15/input.txt", 1)
     print(result)
--- src/day15/test_day15.py
+++ src/day15/test_day15.py
@@ -1,23 +1,22 @@
 import pytest
 
-from src.day15.part01 import get_part_one_result
-from src.day15.part02 import get_part_two_result
+from src.day15.common import process
 
 
 class TestDay15:
     def test_part01_sample01(self):
-        result = get_part_one_result("src/day15/input_sample01.txt")
+        result = process("src/day15/input_sample01.txt", 1)
         assert result == 40
 
     def test_part01(self):
-        result = get_part_one_result("src/day15/input.txt")
+        result = process("src/day15/input.txt", 1)
         assert result == 595
 
     def test_part01_sample02(self):
-        result = get_part_two_result("src/day15/input_sample01.txt", 5)
+        result = process("src/day15/input_sample01.txt", 5)
         assert result == 315
 
     @pytest.mark.skip("long-running test")
     def test_part02(self):
-        result = get_part_two_result("src/day15/input.txt")
+        result = process("src/day15/input.txt", 5)
         assert result == 2914
