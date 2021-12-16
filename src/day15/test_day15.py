73063a1a9beff3ed95d41bc37ab5cfe0715e6878
tomha <tomhaug75@gmail.com>
2021-12-15 20:57:57 -0500

    day 15 part02

--- src/day15/test_day15.py
+++ src/day15/test_day15.py
@@ -1,4 +1,7 @@
+import pytest
+
 from src.day15.part01 import get_part_one_result
+from src.day15.part02 import get_part_two_result
 
 
 class TestDay15:
@@ -9,3 +12,12 @@ class TestDay15:
     def test_part01(self):
         result = get_part_one_result("src/day15/input.txt")
         assert result == 595
+
+    def test_part01_sample02(self):
+        result = get_part_two_result("src/day15/input_sample01.txt", 5)
+        assert result == 315
+
+    @pytest.mark.skip("long-running test")
+    def test_part02(self):
+        result = get_part_two_result("src/day15/input.txt")
+        assert result == 2914
