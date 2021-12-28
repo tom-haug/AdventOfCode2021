31873cbde12467ff70c9bcf629b023941dcbc0d5
tomha <tomhaug75@gmail.com>
2021-12-27 21:06:31 -0500

    day23 part01

--- src/shared/point.py
+++ src/shared/point.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
 from attr import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class Point:
     x: int
     y: int
 
-    def extend(self, new_x: int, new_y: int):
-        if new_x > self.x:
-            self.x = new_x
-        if new_y > self.y:
-            self.y = new_y
+    # def extend(self, new_x: int, new_y: int):
+    #     if new_x > self.x:
+    #         self.x = new_x
+    #     if new_y > self.y:
+    #         self.y = new_y
 
+    def adjacent_to(self, other: Point):
+        delta_x = abs(self.x - other.x)
+        delta_y = abs(self.y - other.y)
+        return delta_x + delta_y == 1
 
 @dataclass
 class Point3D(Point):
